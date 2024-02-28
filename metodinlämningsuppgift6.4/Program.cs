8f0a6228028036c0d2e2a51eaee4e7afefb99e4a
DommSite <johan.johansson06@live.se>
2024-02-28 10:57:36 +0100

    Add project files.

---
+++ metodinlämningsuppgift6.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace potensprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket tal vill du ha som grund?");
+            int grund = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilket tal vill du ha som potens?");
+            int potens = int.Parse(Console.ReadLine());
+            potenser(grund, potens);
+        }
+
+        static void potenser(int grund, int potens)
+        {
+            Console.WriteLine("Kommit");
+            int svar = grund;
+            for(int i = 1; i < potens; i++)
+            {
+                svar = svar * grund;
+            }
+            Console.WriteLine("Svaret blir " + svar);
+        }
+    }
+}
